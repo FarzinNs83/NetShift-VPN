4e3f76607ed52297bc83331bcbc4a5035b2b7830
FarzinNs83 <farzinns83@gmail.com>
2024-10-22 18:17:24 +0330

    Engine

---
+++ lib/engie/vpn_engine.dart
@@ -0,0 +1,5 @@
+class VpnEngine {
+  static const String eventChannelVpnStage = "vpnStage";
+  static const String eventChannelVpnStatus = "vpnStatus";
+  static const String methoedChannelVpnControl = "vpnControl";
+}
